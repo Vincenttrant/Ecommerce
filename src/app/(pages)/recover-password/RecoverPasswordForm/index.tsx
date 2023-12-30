900e8f6b4c9c9e6a47dde786c03d133cb772c506
Vincent Tran <tranvincent73@gmail.com>
2023-12-30 12:25:33 -0600

    implement ecommerce functionality

--- src/app/(pages)/recover-password/RecoverPasswordForm/index.tsx
+++ src/app/(pages)/recover-password/RecoverPasswordForm/index.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import React, { Fragment, useCallback, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import React, {Fragment, useCallback, useState} from 'react'
+import {useForm} from 'react-hook-form'
 import Link from 'next/link'
 
-import { Button } from '../../../_components/Button'
-import { Input } from '../../../_components/Input'
-import { Message } from '../../../_components/Message'
+import {Button} from '../../../_components/Button'
+import {Input} from '../../../_components/Input'
+import {Message} from '../../../_components/Message'
 
 import classes from './index.module.scss'
 
@@ -21,7 +21,7 @@ export const RecoverPasswordForm: React.FC = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: {errors},
   } = useForm<FormData>()
 
   const onSubmit = useCallback(async (data: FormData) => {
@@ -50,32 +50,25 @@ export const RecoverPasswordForm: React.FC = () => {
     <Fragment>
       {!success && (
         <React.Fragment>
-          <h1>Recover Password</h1>
-          <div className={classes.formWrapper}>
-            <p>
-              {`Please enter your email below. You will receive an email message with instructions on
-              how to reset your password. To manage your all users, `}
-              <Link href="/admin/collections/users">login to the admin dashboard</Link>
-              {'.'}
-            </p>
-            <form onSubmit={handleSubmit(onSubmit)} className={classes.form}>
-              <Message error={error} className={classes.message} />
-              <Input
-                name="email"
-                label="Email Address"
-                required
-                register={register}
-                error={errors.email}
-                type="email"
-              />
-              <Button
-                type="submit"
-                appearance="primary"
-                label="Recover Password"
-                className={classes.submit}
-              />
-            </form>
-          </div>
+          <p>Enter your registered email address. We'll send you a code to reset your password.</p>
+
+          <form onSubmit={handleSubmit(onSubmit)} className={classes.form}>
+            <Message error={error} className={classes.message} />
+            <Input
+              name="email"
+              label="Email Address"
+              required
+              register={register}
+              error={errors.email}
+              type="email"
+            />
+            <Button
+              type="submit"
+              appearance="primary"
+              label="Recover Password"
+              className={classes.submit}
+            />
+          </form>
         </React.Fragment>
       )}
       {success && (
